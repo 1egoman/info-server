114026ef6d1d5928c4cb641b06014cb00f3a04e8
Ryan Gaus <rsg1egoman@gmail.com>
2018-03-02 06:29:16 -0500

    chore: old go stuff

--- src/main.go
+++ src/main.go
@@ -4,6 +4,21 @@ import (
   "fmt"
 )
 
+type CalculateMethod func() (interface{}, error)
+
+func CalculateAll(data map[string]interface{}, methods map[string]CalculateMethod) error {
+  for key, fn := range methods {
+    value, err := fn()
+    if err != nil {
+      data[key] = value
+    } else {
+      return err
+    }
+  }
+
+  return nil
+}
+
 type DatumKind string
 
 const (
@@ -13,37 +28,73 @@ const (
 )
 
 var currentDatumIndex int = 0
-type Datum struct {
-  Kind DatumKind
 
-  Index int
-  Next *Datum
-  Prev *Datum
+type Datum interface {
+  Kind() DatumKind
+  Build()
 
-  Data map[string]interface{}
+  Index() int
+  Next() *Datum
+  Previous() *Datum
+
+  Calculate() error
 }
 
-func (d *Datum) LinkNext(n *Datum) {
-  d.Next = n
+
+
+
+
+type TextDatum struct {
+  kind DatumKind
+
+  index int
+  next *Datum
+  prev *Datum
+
+  data map[string]interface{}
 }
 
+func (t *TextDatum) Kind() DatumKind { return t.kind }
+func (t *TextDatum) Build(input interface{}, data map[string]interface{}) {
+  t.data = map[string]interface{}{}
+}
+func (t *TextDatum) Index() int { return t.index }
+func (t *TextDatum) Next() *Datum { return t.next }
+func (t *TextDatum) Previous() *Datum { return t.prev }
+
+func (t *TextDatum) Calculate() error {
+  return CalculateAll(t.data, map[string]CalculateMethod {
+    "TextLength": t.CalculateTextLength,
+  })
+}
+
+func (t *TextDatum) CalculateTextLength() (interface{}, error) {
+  return len(t.data["Input"].(string)), nil
+}
+
+
+
+
+
+var DATUM_TYPES map[DatumKind]Datum = map[DatumKind]Datum{
+  TEXT: &TextDatum{},
+}
 
 var currentDatum *Datum = nil
 
 
 func Injest(kind DatumKind, input interface{}) {
   currentDatumIndex += 1
-  newDatum := &Datum{
-    Kind: kind,
 
-    Index: currentDatumIndex,
-    Next: nil,
-    Prev: currentDatum,
+  // Make a copy of the empty struct that matches the `kind` of this datum, and set it up for use
+  newDatum := DATUM_TYPES[kind]
+  newDatum.Build(input, map[string]interface{}{
+    "kind": kind,
 
-    Data: map[string]interface{}{
-      "Input": input,
-    },
-  }
+    "index": currentDatumIndex,
+    "next": nil,
+    "prev": currentDatum,
+  })
 
   // Link to the next datum from the previous datum
   if currentDatum != nil {
